59d34fc1ae056f782900c587c78d162bdda8d66d
Camilo Pinzon <pinzonac@gmail.com>
2022-03-14 15:30:39 -0500

    add prettier

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
-const http = require('http');
-const url = require('url');
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
-const slugify = require('slugify');
+const slugify = require("slugify");
 
-const replaceTemplate = require('./modules/replaceTemplate');
+const replaceTemplate = require("./modules/replaceTemplate");
 
 // ********** FILES WRITE AND READ ********** //
 // Blocking, Aynchronous way
@@ -30,58 +30,58 @@ const replaceTemplate = require('./modules/replaceTemplate');
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
-  'utf-8'
+  "utf-8"
 );
 
 const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
-  'utf-8'
+  "utf-8"
 );
 const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
-  'utf-8'
+  "utf-8"
 );
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
-const slugs = dataObj.map(slug => slugify(slug.productName, {lower: true}));
+const slugs = dataObj.map((slug) => slugify(slug.productName, { lower: true }));
 
 const server = http.createServer((req, res) => {
-  const {query, pathname} = url.parse(req.url, true);
+  const { query, pathname } = url.parse(req.url, true);
   switch (pathname) {
-    case '/':
-    case '/overview':
+    case "/":
+    case "/overview":
       res.writeHead(200, {
-        'Content-type': 'text/html',
-      })
+        "Content-type": "text/html",
+      });
       const cardsHtml = dataObj
         .map((el) => replaceTemplate(tempCard, el))
-        .join('')
-      res.end(tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml))
-      break
-    case '/product':
+        .join("");
+      res.end(tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml));
+      break;
+    case "/product":
       const product = dataObj[query.id];
       res.writeHead(200, {
-        'Content-type': 'text/html',
-      })
+        "Content-type": "text/html",
+      });
       res.end(replaceTemplate(tempProduct, product));
-      break
-    case '/api':
+      break;
+    case "/api":
       res.writeHead(200, {
-        'Content-type': 'application/json',
-      })
-      res.end(dataObj)
-      break
+        "Content-type": "application/json",
+      });
+      res.end(dataObj);
+      break;
     default:
       res.writeHead(404, {
-        'Content-type': 'text/html',
-      })
-      res.end('<h1>404 Page not found!</h1>')
-      break
+        "Content-type": "text/html",
+      });
+      res.end("<h1>404 Page not found!</h1>");
+      break;
   }
-})
+});
 
-server.listen(8000, '127.0.0.1', () => {
-  console.log('Listening to request on port 8000')
-})
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
+});
