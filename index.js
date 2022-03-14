5dccde872b499d80ec74958337c7e0c699cc8ddd
Camilo Pinzon <pinzonac@gmail.com>
2022-03-14 14:30:07 -0500

    Using 3rd party modules - slugify

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+
+const slugify = require('slugify');
+
 const replaceTemplate = require('./modules/replaceTemplate');
 
 // ********** FILES WRITE AND READ ********** //
@@ -28,17 +31,21 @@ const replaceTemplate = require('./modules/replaceTemplate');
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   'utf-8'
-)
+);
+
 const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
   'utf-8'
-)
+);
 const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
   'utf-8'
-)
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
-const dataObj = JSON.parse(data)
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(slug => slugify(slug.productName, {lower: true}));
 
 const server = http.createServer((req, res) => {
   const {query, pathname} = url.parse(req.url, true);
